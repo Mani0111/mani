c369378321eff3ce7a5cf80cdfce864bc09c3c41
Mani0111 <manikandanthala23@gmail.com>
2016-08-26 10:15:13 +0530

    Stt.java

---
+++ Stt.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+
+public class Stt {
+
+	
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.nextLine();
+		if(s1.equals("Sunday")  || s1.equals("Monday") || s1.equals("Tuesday"))
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+	
+          
+	}
+
+}
